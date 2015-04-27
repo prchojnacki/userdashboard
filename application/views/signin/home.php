fdf93d3039d2d9905ceb16956b0a45418ff7096b
Shain Lafazan <srslafazan@gmail.com>
2015-04-27 13:57:14 -0700

    added main view folders, text for home.php

---
+++ application/views/signin/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+<body>
+<div id='container'>
+    <div id='nav'>
+        <div class='row'>
+            <h3>Test App</h3>
+            <a href='/'>Home</a>
+            <a href='/'>Sign In</a>
+        </div>
+    </div>
+    <div class='row'>
+        <h1>Welcome to the Test!</h1>
+        <p>We're going to build a cool applicatin using a MVC framework! This application was built with the Village88 folks!</p>
+        <button><a href='/'>Start</a></button>
+    </div>
+    <div class='row'>
+        <div class='col-md-4'>
+            <h5>Manage Users</h5>
+            <p>Using this application, you'll learn how to add, remove, and edit users for the application.</p>
+        </div>
+        <div class='col-md-4'>
+            <h5>Leave messages</h5>
+            <p>Users will be able to leave a message to another user using this application.</p>
+        </div>
+        <div class='col-md-4'>
+            <h5>Edit User Information</h5>
+            <p>Admins will ge able to edit another user's information (email address, first name, last name, etc)</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
