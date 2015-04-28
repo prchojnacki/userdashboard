355684d543a02cb22f29ec57a8f3f8a3b3de2e33
Paula Chojnacki <chojnacki.paula@gmail.com>
2015-04-28 15:39:58 -0700

    added css styling to manage_users.php, edit_user.php

--- application/views/admindashboard/edit_user.php
+++ application/views/admindashboard/edit_user.php
@@ -11,41 +11,80 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel='stylesheet' href='/assets/main.css'>
 </head>
 <body>
-	<div id='container'>
-		<div id='nav'>
-			<div class='row'>
-				<a href="">Test App</a>
-				<a href='/admindashboard/index'>Dashboard</a>
-				<a href="/normaldashboard/editprofile">Profile</a>
-				<a href="/normaldashboard/view_wall/<?=$this->session->userdata("userid")?>">Wall</a>
-				<a href="/signins/logoff">Log off</a>
-			</div>
+	<nav class='navbar navbar-default'>
+    <div class='collapse navbar-collapse'>
+        <ul class='nav navbar-nav'>
+            <li><a href=''>Test App</a></li>
+            <li><a href='/admindashboard/index'>Dashboard</a></li>
+            <li><a href="/normaldashboard/editprofile">Profile</a></li>
+            <li><a href="/normaldashboard/view_wall/<?=$this->session->userdata("userid")?>">Wall</a></li>
+        </ul>
+        <ul class='nav navbar-nav navbar-right'>
+            <li><a href="/signins/logoff">Log off</a></li>
+        </ul>
+    </div>
+	</nav>
+	<div class='container-fluid'>
+		<div class='row'>
+			<h1 class='col-sm-9 col-sm-offset-1'>Edit user #<?=$id?></h1>
+			<form action='/admindashboard/index' method='post' class='col-sm-1'>
+				<input class = 'btn btn-primary' type='submit' value='Return to Dashboard'>
+			</form>
 		</div>
-		<h1>Edit user #<?=$id?></h1>
-		<form action='/admindashboard/index' method='post'>
-			<input class = 'btn btn-primary' type='submit' value='Return to Dashboard'>
-		</form>
-		<h3>Edit Information</h3>
-		<form action='/admindashboard/change_user_info' method='post'>
-			<input type='hidden' name='id' value='<?=$id?>'>
-			<label>Email Address: <input type='text' name='email' value='<?=$user["email"]?>'></label>
-			<label>First Name: <input type='text' name='first_name' value='<?=$user["first_name"]?>'></label>
-			<label>Last Name: <input type='text' name='last_name' value='<?=$user["last_name"]?>'></label>
-			<label>User Level: <select class='form-control' name='level'>
-				<option value = 'normal'>Normal</option>
-				<option value = 'admin'>Admin</option>
-			</select></label>
-			<input class = 'btn btn-success' type='submit' value = 'Save'>
-		</form>
-		<h3>Change Password</h3>
-		<form action='/admindashboard/change_user_password' method='post'>
-			<input type='hidden' name='id' value='<?=$id?>'>
-			<label>Password: <input type='text' name='password'></label>
-			<label>Confirm Password: <input type='text' name='confirm'></label>
-			<input class = 'btn btn-success' type='submit' value = 'Update Password'>
-		</form>
+		<div class='row'>
+			<h3 class='col-sm-offset-1'>Edit Information</h3>
+			<form action='/admindashboard/change_user_info' method='post' class='form-horizontal'>
+				<input type='hidden' name='id' value='<?=$id?>'>
+				<div class='form-group'>
+					<label class='col-sm-2 control-label'>Email Address: </label>
+					<div class='col-sm-4'>
+						<input type='text' name='email' value='<?=$user["email"]?>' class='form-control'>
+					</div>
+				</div>
+				<div class='form-group'>
+					<label class='col-sm-2 control-label'>First Name: </label>
+					<div class='col-sm-4'>
+						<input type='text' name='first_name' value='<?=$user["first_name"]?>' class='form-control'>
+					</div>
+				</div>
+				<div class='form-group'>
+					<label class='col-sm-2 control-label'>Last Name: </label>
+					<div class='col-sm-4'>
+						<input type='text' name='last_name' value='<?=$user["last_name"]?>' class='form-control'>
+					</div>
+				</div>
+				<div class='form-group'>
+					<label class='col-sm-2 control-label'>User Level: </label>
+					<div class='col-sm-1'>
+						<select class='form-control' name='level'>
+							<option value = 'normal'>Normal</option>
+							<option value = 'admin'>Admin</option>
+						</select>
+					</div>
+				</div>
+				<div class='form-group'>
+					<input class = 'btn btn-success col-sm-offset-2' type='submit' value = 'Save'>
+				</div>
+			</form>
+			<h3 class='col-sm-offset-1'>Change Password</h3>
+			<form action='/admindashboard/change_user_password' method='post' class='form-horizontal'>
+					<input type='hidden' name='id' value='<?=$id?>'>
+				<div class='form-group'>
+					<label class='col-sm-2 control-label'>Password: </label>
+					<input type='text' name='password'>
+				</div>
+				<div class='form-group'>
+					<label class='col-sm-2 control-label'>Confirm Password: </label>
+					<input type='text' name='confirm'>
+				</div>
+				<div class='form-group'>
+					<input class = 'btn btn-success col-sm-offset-2' type='submit' value = 'Update Password'>
+				</div>
+			</form>
+		</div>
 	</div>
 </body>
 </html>
--- application/views/admindashboard/manage_users.php
+++ application/views/admindashboard/manage_users.php
@@ -11,55 +11,66 @@
 
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<link rel='stylesheet' href='/assets/main.css'>
 </head>
 <body>
+	<nav class='navbar navbar-default'>
+    <div class='collapse navbar-collapse'>
+        <ul class='nav navbar-nav'>
+            <li><a href=''>Test App</a></li>
+            <li><a href='/admindashboard/index'>Dashboard</a></li>
+            <li><a href="/normaldashboard/editprofile">Profile</a></li>
+            <li><a href="/normaldashboard/view_wall/<?=$this->session->userdata("userid")?>">Wall</a></li>
+        </ul>
+        <ul class='nav navbar-nav navbar-right'>
+            <li><a href="/signins/logoff">Log off</a></li>
+        </ul>
+    </div>
+	</nav>
 	<div id='container'>
-		<div id='nav'>
-			<div class='row'>
-				<a href="">Test App</a>
-				<a href='/admindashboard/index'>Dashboard</a>
-				<a href="/normaldashboard/editprofile">Profile</a>
-				<a href="/normaldashboard/view_wall/<?=$this->session->userdata("userid")?>">Wall</a>
-				<a href="/signins/logoff">Log off</a>
+		<div class='row'>
+			<h1 class='col-sm-9 col-sm-offset-1'>Manage Users</h1>
+			<form action='/admindashboard/newuser' method='post' class='col-sm-1'>
+				<input class = 'btn btn-primary' type='submit' value='Add New'>
+			</form>
+		</div>
+		<div class='row'>
+			<div class='col-sm-10 col-sm-offset-1'>
+				<table class='table table-striped table-bordered'>
+					<thead>
+						<tr>
+							<th>ID</th>
+							<th>Name</th>
+							<th>Email</th>
+							<th>Created At</th>
+							<th>User Level</th>
+							<th>Actions</th>
+						</tr>
+					</thead>
+					<tbody>
+			<?php  
+					foreach($userquery as $user) {
+			 ?>		
+						<tr>
+							<td><?= $user['id'] ?></td>
+							<td><a href='/normaldashboard/view_wall/<?= $user['id'] ?>'><?= $user['first_name'] . " " . $user['last_name'] ?></a></td>
+							<td><?= $user['email'] ?></td>
+							<td><?= $user['created_at'] ?></td>
+							<td><?php
+								if( $user['user_level'] == 0) {
+									echo "normal";
+								} else echo "admin";
+							?></td>
+							<td><a href="/admindashboard/edit_user/<?= $user['id']?>">edit</a> <a href="/admindashboard/remove_user/<?= $user['id'] ?>">remove</a></td>
+						</tr>
+
+			<?php
+				}
+			?>
+					</tbody>
+				</table>
 			</div>
 		</div>
-		<h1>Manage Users</h1>
-		<form action='/admindashboard/newuser' method='post'>
-			<input class = 'btn btn-primary' type='submit' value='Add New'>
-		</form>
-		<table class='table table-striped'>
-			<thead>
-				<tr>
-					<th>ID</th>
-					<th>Name</th>
-					<th>Email</th>
-					<th>Created At</th>
-					<th>User Level</th>
-					<th>Actions</th>
-				</tr>
-			</thead>
-			<tbody>
-	<?php  
-			foreach($userquery as $user) {
-	 ?>		
-				<tr>
-					<td><?= $user['id'] ?></td>
-					<td><a href='/normaldashboard/view_wall/<?= $user['id'] ?>'><?= $user['first_name'] . " " . $user['last_name'] ?></a></td>
-					<td><?= $user['email'] ?></td>
-					<td><?= $user['created_at'] ?></td>
-					<td><?php
-						if( $user['user_level'] == 0) {
-							echo "normal";
-						} else echo "admin";
-					?></td>
-					<td><a href="/admindashboard/edit_user/<?= $user['id']?>">edit</a> <a href="/admindashboard/remove_user/<?= $user['id'] ?>">remove</a></td>
-				</tr>
-
-	<?php
-		}
-	?>
-			</tbody>
-		</table>
 	</div>
 
 
