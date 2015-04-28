e2ab8b00828b54c1e93533d5c1e9d4380ffd37fa
Shain Lafazan <srslafazan@gmail.com>
2015-04-28 16:34:38 -0700

    changes to UI for admindashboard and normaldashboard, edit_userphp and editprofile.php. altered main.css

--- application/views/admindashboard/edit_user.php
+++ application/views/admindashboard/edit_user.php
@@ -14,7 +14,7 @@
 	<link rel='stylesheet' href='/assets/main.css'>
 </head>
 <body>
-	<nav class='navbar navbar-default'>
+<nav class='navbar navbar-default'>
     <div class='collapse navbar-collapse'>
         <ul class='nav navbar-nav'>
             <li><a href=''>Test App</a></li>
@@ -26,7 +26,8 @@
             <li><a href="/signins/logoff">Log off</a></li>
         </ul>
     </div>
-	</nav>
+</nav>
+
 	<div class='container-fluid'>
 		<div class='row'>
 			<h1 class='col-sm-9 col-sm-offset-1'>Edit user #<?=$id?></h1>
@@ -34,56 +35,66 @@
 				<input class = 'btn btn-primary' type='submit' value='Return to Dashboard'>
 			</form>
 		</div>
+
 		<div class='row'>
-			<h3 class='col-sm-offset-1'>Edit Information</h3>
-			<form action='/admindashboard/change_user_info' method='post' class='form-horizontal'>
-				<input type='hidden' name='id' value='<?=$id?>'>
-				<div class='form-group'>
-					<label class='col-sm-2 control-label'>Email Address: </label>
-					<div class='col-sm-4'>
-						<input type='text' name='email' value='<?=$user["email"]?>' class='form-control'>
-					</div>
-				</div>
-				<div class='form-group'>
-					<label class='col-sm-2 control-label'>First Name: </label>
-					<div class='col-sm-4'>
-						<input type='text' name='first_name' value='<?=$user["first_name"]?>' class='form-control'>
-					</div>
-				</div>
-				<div class='form-group'>
-					<label class='col-sm-2 control-label'>Last Name: </label>
-					<div class='col-sm-4'>
-						<input type='text' name='last_name' value='<?=$user["last_name"]?>' class='form-control'>
-					</div>
-				</div>
-				<div class='form-group'>
-					<label class='col-sm-2 control-label'>User Level: </label>
-					<div class='col-sm-1'>
-						<select class='form-control' name='level'>
-							<option value = 'normal'>Normal</option>
-							<option value = 'admin'>Admin</option>
-						</select>
-					</div>
-				</div>
-				<div class='form-group'>
-					<input class = 'btn btn-success col-sm-offset-2' type='submit' value = 'Save'>
-				</div>
-			</form>
-			<h3 class='col-sm-offset-1'>Change Password</h3>
-			<form action='/admindashboard/change_user_password' method='post' class='form-horizontal'>
+			<div class='col-sm-4 col-sm-offset-1'>
+				<h3 class='col-sm-offset-1'>Edit Information</h3>
+				<form action='/admindashboard/change_user_info' method='post' class='form-horizontal'>
 					<input type='hidden' name='id' value='<?=$id?>'>
-				<div class='form-group'>
-					<label class='col-sm-2 control-label'>Password: </label>
-					<input type='text' name='password'>
-				</div>
-				<div class='form-group'>
-					<label class='col-sm-2 control-label'>Confirm Password: </label>
-					<input type='text' name='confirm'>
-				</div>
-				<div class='form-group'>
-					<input class = 'btn btn-success col-sm-offset-2' type='submit' value = 'Update Password'>
-				</div>
-			</form>
+					<div class='form-group col-sm-12'>
+						<label class='col-sm-4 control-label'>Email Address: </label>
+						<div class='col-sm-6'>
+							<input type='text' name='email' value='<?=$user["email"]?>' class='form-control'>
+						</div>
+					</div>
+					<div class='form-group col-sm-12'>
+						<label class='col-sm-4 control-label'>First Name: </label>
+						<div class='col-sm-6'>
+							<input type='text' name='first_name' value='<?=$user["first_name"]?>' class='form-control'>
+						</div>
+					</div>
+					<div class='form-group col-sm-12'>
+						<label class='col-sm-4 control-label'>Last Name: </label>
+						<div class='col-sm-6'>
+							<input type='text' name='last_name' value='<?=$user["last_name"]?>' class='form-control'>
+						</div>
+					</div>
+					<div class='form-group col-sm-12'>
+						<label class='col-sm-4 control-label'>User Level: </label>
+						<div class='col-sm-4'>
+							<select class='form-control' name='level'>
+								<option value = 'normal'>Normal</option>
+								<option value = 'admin'>Admin</option>
+							</select>
+						</div>
+					</div>
+					<div class='form-group col-sm-7'>
+						<input class = 'btn btn-success col-sm-offset-2' type='submit' value = 'Save'>
+					</div>
+				</form>
+			</div>
+
+			<div class='col-sm-5'>
+				<h3 class='col-sm-offset-1'>Change Password</h3>
+				<form action='/admindashboard/change_user_password' method='post' class='form-horizontal'>
+						<input type='hidden' name='id' value='<?=$id?>'>
+					<div class='form-group col-sm-12'>
+						<label class='col-sm-4 control-label'>Password: </label>
+						<div class='col-sm-5'>
+							<input class='form-control' type='text' name='password'>
+						</div>
+					</div>
+					<div class='form-group col-sm-12'>
+						<label class='col-sm-4 control-label'>Confirm Password: </label>
+						<div class='col-sm-5'>
+							<input class='form-control' type='text' name='confirm'>
+						</div>
+					</div>
+					<div class='form-group col-sm-12'>
+						<input class = 'btn btn-success col-sm-offset-2' type='submit' value = 'Update Password'>
+					</div>
+				</form>
+			</div>
 		</div>
 	</div>
 </body>
--- application/views/normaldashboard/editprofile.php
+++ application/views/normaldashboard/editprofile.php
@@ -10,52 +10,94 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<link rel='stylesheet' href='/assets/main.css'>
 </head>
 <body>
-<div id='container'>
-    <div id='nav'>
-        <div class='row'>
-            <h3>Test App</h3>
-        <?php
-            if ($this->session->userdata('admin') == 'admin') {
-        ?>
-            <a href='/admindashboard/index'>Home</a>
-        <?php
+<nav class='navbar navbar-default'>
+    <div class='collapse navbar-collapse'>
+        <ul class='nav navbar-nav'>
+            <li><a href=''>Test App</a></li>
+            <?php
+                if ($this->session->userdata('admin') == 'admin') {
+            ?>
+            <li><a href='/admindashboard/index'>Dashboard</a></li>
+            <?php
             } else {
-        ?>
+            ?>
             <a href='/normaldashboard/index'>Home</a>
-        <?php
+            <?php
             }
-        ?>
-            <a href='/signins/logoff'>Log Off</a>
-            <a href='/normaldashboard/editprofile'>Profile</a>
-            <a href='/normaldashboard/view_wall/<?=$this->session->userdata("userid")?>'>Wall</a>
+            ?>
+            <li><a href="/normaldashboard/editprofile">Profile</a></li>
+            <li><a href="/normaldashboard/view_wall/<?=$this->session->userdata("userid")?>">Wall</a></li>
+        </ul>
+        <ul class='nav navbar-nav navbar-right'>
+            <li><a href="/signins/logoff">Log off</a></li>
+        </ul>
+    </div>
+</nav>
+<div class='container-fluid'>
+        
+    <div class='row'>
+        <h1 class='col-sm-9 col-sm-offset-1'>Edit profile</h1>
+        <form action='/normaldashbard/index' method='post' class='col-sm-1'>
+            <input class = 'btn btn-primary' type='submit' value='Return to Dashboard'>
+        </form>
+    </div>
+
+    <div class='row'>
+        <div class='col-sm-4 col-sm-offset-1'>
+            <h3 class='col-sm-offset-1'>Edit Information</h3>
+            <form action='/normaldashboard/update_profile' method='post' class='form-horizontal'>
+                <div class='col-sm-12 form-group'>     
+                    <label class='col-sm-4 control-label'>Email: </label>
+                    <div class='col-sm-6'>
+                        <input class='form-control' type='email' name='email'>
+                    </div>
+                </div>
+                <div class='col-sm-12 form-group'>
+                    <label class='col-sm-4 control-label'>First Name: </label>
+                    <div class='col-sm-6'>
+                        <input class='form-control' type='text' name='first_name'>
+                    </div>
+                </div>
+                <div class='col-sm-12 form-group'>
+                    <label class='col-sm-4 control-label'>Last Name: </label>
+                    <div class='col-sm-6'>
+                        <input class='form-control' type='text' name='last_name'>
+                    </div>
+                </div>
+                <div class='col-sm-10 form-group descriptionBox'>
+                    <label for='description'>Edit Description: </label>
+                    <textarea id='description' class='form-control' name='description'></textarea>
+                </div>
+                <div class='form-group col-sm-12'>
+                    <input class='btn btn-success col-sm-offset-8' type='submit' value='Save'>
+                </div>
+
+            </form>
+        </div>
+        <div class='col-md-6'>
+            <h3 class='col-sm-offset-1'>Change Password</h3>
+            <form action='/normaldashboard/update_password' method='post' class='form-horizontal'>
+                <div class='form-group col-sm-12'>
+                    <label class='col-sm-4 control-label'>Password: </label>
+                    <div class='col-sm-5'>
+                        <input class='form-control' type='text' name='password'>
+                    </div>
+                </div>
+                <div class='form-group col-sm-12'>
+                    <label class='col-sm-4 control-label'>Confirm Password: </label>
+                    <div class='col-sm-5'>
+                        <input class='form-control' type='text' name='confirm'>
+                    </div>
+                </div>
+                <div class='form-group col-sm-12'>
+                    <input class='btn btn-success col-sm-offset-2' type='submit' value='Update Password'>
+                </div>
+            </form>
         </div>
     </div>
-    <h1>Edit profile</h1>
-<div class='row'>
-    <div class='col-md-6'>
-        <form action='/normaldashboard/update_profile' method='post'>
-            <h5>Edit Information</h5>
-            <label>Email:<input type='email' name='email'></label>
-            <label>First Name:<input type='text' name='first_name'></label>
-            <label>Last Name:<input type='text' name='last_name'></label>
-            <label>Edit Description<textarea name='description'></textarea></label>
-            <input type='submit' value='Save'>
-        </form>
-    </div>
-    <div class='col-md-6'>
-        <form action='/normaldashboard/update_password' method='post'>
-            <h5>Change Password</h5>
-            <label>Password:<input type='text' name='password'></label>
-            <label>Password Confirmation:<input type='text' name='confirm'></label>
-            <input type='submit' value='Update Password'>
-        </form>
-    </div>
-</div>
-
-
-
 </div>
 </body>
 </html>
